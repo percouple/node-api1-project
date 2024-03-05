9ab1f241dac2e5074eeb91a283d6e190b23ecc6e
Percouple <calebsevan@gmail.com>
2024-03-05 09:22:56 -0600

    Finished challenges

--- api/server.js
+++ api/server.js
@@ -5,6 +5,19 @@ server.use(express.json())
 
 const User = require('./users/model')
 
+// [POST] A new user
+server.post('/api/users', async (req, res) => {
+    try {
+        console.log("SUCCESS ROUTING")
+        if (!req.body.name || !req.body.bio) {
+            return res.status(400).json({ message: "provide name and bio" })
+        }
+        const newUser = await User.insert({ name: req.body.name, bio: req.body.bio })
+        res.status(201).json(newUser)
+    } catch (err) {
+        res.status(500).json("Internal Server Error")
+    }
+})
 
 // [GET] All Users
 server.get('/api/users', async (req, res) => {
@@ -18,15 +31,50 @@ server.get('/api/users', async (req, res) => {
 })
 
 // [GET] Individual Users
-server.get('/api/users', async (req, res) => {
-    try{
-        
+server.get('/api/users/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await User.findById(id)
+        if (!user) {
+            return res.status(404).json({ message: "does not exist" })
+        }
+        res.status(200).json(user);
     } catch (err) {
-
+        res.status(500).json("Internal Server Error")
     }
 })
 
-console.log(User.find())
+// [DELETE] A user
+server.delete('/api/users/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        let user = await User.remove(id)
+        if (!user) {
+            console.log("User not found")
+            return res.status(404).json({ message: 'does not exist' });
+        }
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json("Internal Server Error")
+    }
+})
+
+// [PUT] Update a user
+server.put('/api/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    try {
+        const user = await User.update(id, changes)
+        if (!user) {
+            return res.status(404).json({ message: "does not exist"})
+        } else if (!user.name || !user.bio) {
+            return res.status(400).json({ message: "provide name and bio"})
+        }
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(500).json("Internal Server Error")
+    }
+})
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
